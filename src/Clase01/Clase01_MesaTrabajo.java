6e074714dc6600b936df291fd0dc8977515cd243
tomasdlp <tomidlp9531@gmail.com>
2023-02-15 18:36:10 -0300

    Clase01_MesaTrabajo

---
+++ src/Clase01/Clase01_MesaTrabajo.java
@@ -0,0 +1,61 @@
+package Clase01;
+
+public class Clase01_MesaTrabajo {
+    public static void main(String[] args) {
+        String animal;
+        String nombre;
+        int edad;
+        String CuantoCome;
+        String respuesta;
+
+        animal = "Perro";
+        nombre = "Manchitas";
+        edad = 2;
+        CuantoCome = "1 kilo y medio";
+        respuesta = "Gua Gua!!";
+
+        System.out.println(
+                nombre + " es un " + animal + " que tiene " + edad + " años " + "Come " + CuantoCome + " y hace "+ respuesta
+        );
+
+        animal = "Pez";
+        nombre = "Nemo";
+        edad = 1;
+        CuantoCome = "140 gramos";
+        respuesta = "Glu glu!!";
+
+        System.out.println(
+                nombre + " es un " + animal + " que tiene " + edad + " años " + "Come " + CuantoCome + " y hace "+ respuesta
+        );
+
+        animal = "Gato";
+        nombre = "Silvestre";
+        edad = 3;
+        CuantoCome = "medio kilo";
+        respuesta = "Miau miau!!";
+
+        System.out.println(
+                nombre + " es un " + animal + " que tiene " + edad + " años " + "Come " + CuantoCome + " y hace "+ respuesta
+        );
+
+        animal = "Tortuga";
+        nombre = "Manuelita";
+        edad = 12;
+        CuantoCome = "300 gramos";
+        respuesta = "Pehuajo";
+
+        System.out.println(
+                nombre + " es un " + animal + " que tiene " + edad + " años " + "Come " + CuantoCome + " y hace "+ respuesta
+        );
+
+        animal = "Pajaro";
+        nombre = "Gardel";
+        edad = 1;
+        CuantoCome = "350 gramos";
+        respuesta = "Pio pio pio!!";
+
+        System.out.println(
+                nombre + " es un " + animal + " que tiene " + edad + " años " + "Come " + CuantoCome + " y hace "+ respuesta
+        );
+    }
+}
